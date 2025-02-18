26e7b43005e2c47128c2b6838f9bf6f2df7e05f2
Juan Esteban Correa <correaguzmanjuanesteban@gmail.com>
2025-02-18 10:46:00 -0500

    hicimos la clase note, con los parametro pedidos

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,9 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+
+class Note:
+    HIGH = 'HIGH'
+    MEDIUM = 'MEDIUM'
+    LOW = 'LOW'
+
+    def __init__(self, code: str, title: str, text: str, importance: str):
